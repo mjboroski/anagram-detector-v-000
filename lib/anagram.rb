e4bde0135f34f5667c9e828b7dc8dfac0f78e1be
Michael Boroski <mjboroski@gmail.com>
2018-04-23 19:39:16 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def intiialize(word)
+    @word=word
+  end
+
+  def match(input)
+    matches=[]
+    %w(input).each do |test|
+      matches<<test if test.map.sort == @word.map.sort
+      
+      # compare @word to test and see if they are anagrams
+      
+    end
+    matches
+  end
