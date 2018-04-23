630e2eebfb9304d774af5491fb329e36757bfb39
Michael Boroski <mjboroski@gmail.com>
2018-04-23 19:45:29 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,11 +8,9 @@ class Anagram
 
   def match(input)
     matches=[]
-    %w(input).each do |test|
+    input.each do |test|
       matches<<test if test.map.sort == @word.map.sort
-
       # compare @word to test and see if they are anagrams
-
     end
     matches.flatten
   end
